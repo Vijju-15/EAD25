ef987c0a65353a29eb40d47b7a36c97ffc2e9fe8
Vijju-15 <vijju152005@gmail.com>
2025-12-01 06:27:44 +0530

    added example python file

---
+++ example.py
@@ -0,0 +1,6 @@
+def say_hello(name):
+    print(f"Hello, {name}! Hope you're having a wonderful day 😊")
+
+if __name__ == "__main__":
+    user_name = input("What's your name? ")
+    say_hello(user_name)
